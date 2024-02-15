2d8ca5af4cbf77b29471ce4ae8f2099be18f7716
Darrel <39296145+darrelhong@users.noreply.github.com>
2024-02-15 02:46:07 +0000

    hello world

---
+++ hello-world/hello.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
