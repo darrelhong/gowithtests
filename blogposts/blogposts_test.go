7992007ccf8e5d09f3c1fa9e25f5aede1a2dff52
Darrel <39296145+darrelhong@users.noreply.github.com>
2024-02-21 08:38:00 +0000

    files

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,57 @@
+package blogposts_test
+
+import (
+	"blogposts"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPost(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+A
+New Post
+foo
+`
+	)
+
+	fs := fstest.MapFS{
+		"hello-world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != 2 {
+		t.Errorf("got %d posts, want 2", len(posts))
+	}
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+	World`,
+	})
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
